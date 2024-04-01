03edd6a09db14b3f6d03f264df6c301028dfeee9
CrisMontecinos <cmontecinosf@utem.cl>
2024-04-01 20:00:47 -0300

    Agregado mi código

--- src/pages/config/Config.tsx
+++ src/pages/config/Config.tsx
@@ -1,10 +1,59 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './config.scss';
+
 const Config = () => {
-    return(
-        <div>
-            Config
-        </div>
-    )
-    
-    }
-    
-    export default Config
+    const [pin, setPin] = useState('');
+    const [confirmPin, setConfirmPin] = useState('');
+
+    const handleAccept = () => {
+        // Aquí puedes hacer lo que necesites con los valores guardados
+        console.log('PIN:', pin);
+        console.log('Confirm PIN:', confirmPin);
+        // Por ejemplo, puedes enviar estos valores a una API, guardarlos en localStorage, etc.
+    };
+
+    return (
+        <>
+            <div className="config-container">
+                <h2>Registro</h2>
+                <p>Crea un PIN para usarlo en el <br />
+                registro de tu asistencia</p>
+                <Box 
+                    sx={{
+                        width: 300,
+                        maxWidth: '100%',
+                    }}
+                    className="config-box"
+                >
+                    <TextField
+                        fullWidth
+                        label="Ingrese su PIN"
+                        id="pin"
+                        value={pin}
+                        onChange={(e) => setPin(e.target.value)}
+                    />
+                    <TextField
+                        fullWidth
+                        label="Confirme su PIN"
+                        id="confirmPin"
+                        value={confirmPin}
+                        onChange={(e) => setConfirmPin(e.target.value)}
+                    />
+                    <Stack spacing={2} direction="row" justifyContent="center">
+                        <Button variant="contained" color="secondary" onClick={handleAccept}>Aceptar</Button>
+                        <Link to="../Createpin">
+                        <Button variant="contained" color="secondary">Cancelar</Button>
+                        </Link>
+                    </Stack>
+                </Box>
+            </div>
+        </>
+    );
+};
+
+export default Config;

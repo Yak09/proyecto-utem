a3e3c847c5799dd432583ad785586745b5465e13
CrisMontecinos <cmontecinosf@utem.cl>
2024-04-03 21:54:51 -0300

    Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,48 @@
+import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import QrCodeIcon from '@mui/icons-material/QrCode';
+import '../config/config.scss';
 
-const Home = () => {
-return(
-    <div>
-        <Button variant="contained">Hello world</Button>
-    </div>
-)
+const CreatePin = () => {
+    return (
+        <>
+            <div className="config-container">
 
-}
+                <h3>Registro de Asistenciar</h3> <br/>
+                {/* Utiliza Link para enlazar al botón con la ruta de Config */}
+                <Link to="../Generar">
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        style={{ 
+                            width: '200px', // Personaliza el ancho del botón
+                            height: '50px', // Personaliza la altura del botón
+                            fontSize: '18px', // Personaliza el tamaño de la fuente
+                            margin: '15px',
+                        }} 
+                        startIcon={<QrCodeIcon />} // Coloca el icono al inicio del botón
+                    >
+                        Generar QR
+                    </Button>
+                </Link>
 
-export default Home
+                <Link to="../escanear">
+                    <Button 
+                        variant="contained" 
+                        color="secondary" 
+                        style={{ 
+                            width: '200px', // Personaliza el ancho del botón
+                            height: '50px', // Personaliza la altura del botón
+                            fontSize: '18px', // Personaliza el tamaño de la fuente
+                        }} 
+                        startIcon={<QrCodeIcon />} // Coloca el icono al inicio del botón
+                    >
+                        Escanear QR
+                    </Button>
+                </Link>
+            </div>
+        </>
+    );
+};
+
+export default CreatePin;

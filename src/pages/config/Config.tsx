41c8826155b0ad8c808d1ec4e881dc42187ac0dd
CrisMontecinos <cmontecinosf@utem.cl>
2024-04-03 21:28:50 -0300

    cambios menores

--- src/pages/config/Config.tsx
+++ src/pages/config/Config.tsx
@@ -24,33 +24,35 @@ const Config = () => {
                 <p>Crea un PIN para usarlo en el <br />
                 registro de tu asistencia</p>
                 <Box 
-                    sx={{
-                        width: 300,
-                        maxWidth: '100%',
-                    }}
-                    className="config-box"
-                >
-                    <TextField
-                        fullWidth
-                        label="Ingrese su PIN"
-                        id="pin"
-                        value={pin}
-                        onChange={(e) => setPin(e.target.value)}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Confirme su PIN"
-                        id="confirmPin"
-                        value={confirmPin}
-                        onChange={(e) => setConfirmPin(e.target.value)}
-                    />
-                    <Stack spacing={2} direction="row" justifyContent="center">
-                        <Button variant="contained" color="secondary" onClick={handleAccept}>Aceptar</Button>
-                        <Link to="../Createpin">
-                        <Button variant="contained" color="secondary">Cancelar</Button>
-                        </Link>
-                    </Stack>
-                </Box>
+    sx={{
+        width: 300,
+        maxWidth: '100%',
+    }}
+    className="config-box"
+>
+    <TextField
+        fullWidth
+        label="Ingrese su PIN"
+        id="pin"
+        value={pin}
+        onChange={(e) => setPin(e.target.value)}
+        style={{ marginBottom: '20px' }} // Agregar margen inferior a este TextField
+    />
+    <TextField
+        fullWidth
+        label="Confirme su PIN"
+        id="confirmPin"
+        value={confirmPin}
+        onChange={(e) => setConfirmPin(e.target.value)}
+    />
+    <Stack spacing={2} direction="row" justifyContent="center" style={{ marginTop: '20px' }}> {/* Agregar margen superior a este Stack */}
+        <Button variant="contained" color="secondary" onClick={handleAccept} style={{ width: '150px' }}>Aceptar</Button>
+        <Link to="../Createpin" style={{ textDecoration: 'none' }}> {/* Quité el botón del Link */}
+            <Button variant="contained" color="secondary" style={{ width: '150px' }}>Cancelar</Button>
+        </Link>
+    </Stack>
+</Box>
+
             </div>
         </>
     );

a3830af9d0c3f00dc12ba42b5e686d2c33fddf40
Cristian Montecinos <cmontecinosf@utem.cl>
2024-05-06 17:07:39 -0400

    subjectselection

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ import Perfil from './pages/perfil/Perfil'
 // import Sidebar from './components/Sidebar';
 import Generar from './pages/generar/Generar';
 //import Escanear from './pages//escanear/Escanear';
-import Escanear_copy from './pages//escanear/Escanear_copy';
+import Escanear_copy from './pages/escanear/Escanear_copy';
 import CreatePin from './pages/config/CreatePin';
 import Config from './pages/config/Config';
 import DataGrid from './pages/home/dataGrid';
+import SubjectSelection from './components/SubjectSelection'; // Importa el componente de selección de asignaturas
 // import { Banner } from './components/Banner';
 
 function App() {
@@ -25,7 +26,11 @@ function App() {
             <Route path="/Generar" element={<Generar />} />
             <Route path="/Escanear" element={<Escanear_copy />} />
             <Route path="/CreatePin" element={<CreatePin />} />
-            <Route path="/Config" element={<Config />}/>  
+            <Route path="/Config" element={<Config />}/> 
+            {/* Nueva ruta para la selección de asignaturas */}
+            <Route path="/select-subject" element={<SubjectSelection />} />
+            {/* Ruta dinámica para el DataGrid que recibe el nombre de la asignatura como parámetro */}
+            <Route path="/datagrid/:subject" element={<DataGrid />} />
           </Routes>
     </Router>
     </>

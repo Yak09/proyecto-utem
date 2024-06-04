7042b921f282578697e684b2d53b03e621e57489
Cristian Montecinos <cmontecinosf@utem.cl>
2024-06-04 16:31:16 -0400

    se crean perfiles y se muestran en el appbar

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -7,12 +7,30 @@ const Profile = () => {
     return <div>Loading ...</div>;
   }
 
+  const namespace = 'https://your-namespace.com/'; 
+  const roles = user[namespace + 'roles'] || [];
+
+
   return (
     isAuthenticated && (
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px'}}>
-        <span style={{ fontSize: '14px' }}>{user.name}</span>
-        <span style={{ fontSize: '14px', color: 'gray' }}>{user.email}</span>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '20px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+          <span style={{ fontSize: '14px' }}>{user.name}</span>
+          <span style={{ fontSize: '14px', color: 'gray' }}>{user.email}</span>
+        </div>
         <img src={user.picture} alt={user.name} style={{ borderRadius: '50%', width: '50px', height: '50px' }} />
+        <div>
+          {roles.length > 0 && (
+            <div>
+              <strong>Roles:</strong>
+              <ul>
+                {roles.map((role, index) => (
+                  <li key={index} style={{ fontSize: '14px', color: 'blue' }}>{role}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     )
   );
